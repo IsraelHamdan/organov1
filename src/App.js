2a22c2d159fdc07fd353a0e14ddaae80703f4fa2
Israel Hamdan <IsraelSantos_20k@outlook.com>
2023-01-26 16:04:51 -0300

    aula 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/banner/banner';
+
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bem-vindo ao reactjs <code>src/App.js</code> 
         </p>
         <a
           className="App-link"
---
+++ src/components/banner/banner.js
@@ -0,0 +1,7 @@
+function Banner () {
+   return (
+      <img src = "/imagens/banner.png" alt = "o banner principal do organo" />
+   )
+}
+
+export default Banner; 
